92ec2a372f0681811e36aede96bbad5c18cd8b44
Wedrig <jackkmorice@gmail.com>
2017-02-24 06:59:07 -0600

    Create site.js

---
+++ site.js
@@ -0,0 +1,4 @@
+//Props to github.com/Bax3n for this code!
+document.body.innerHTML='<div class="nav"><a href="//historyas.github.io"><li>History Project</li></a></div> \
+<div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//historyas.github.io/Theme.css">'
+
