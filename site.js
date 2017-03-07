b4b7e700e2dacbbf13a97c3eba000fbdd58d979c
Wedrig <jackkmorice@gmail.com>
2017-03-07 10:27:27 -0600

    Update site.js

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
 //Props to github.com/Bax3n for this code!
-document.body.innerHTML='<div class="nav"><a href="//historyas.github.io"><li>History Project</li></a><a href="//historyas.github.io/geo.html"><li>Geography</li></a><a href="//historyas.github.io/farm.html"><li>Farming</li></a><a href="//historyas.github.io/trade.html"><li>Trading</li></a></div> \
+document.body.innerHTML='<div class="nav"><a href="//historyas.github.io"><li>History Project</li></a><a href="//historyas.github.io/geo.html"><li>Geography</li></a><a href="//historyas.github.io/farm.html"><li>Farming</li></a><a href="//historyas.github.io/trade.html"><li>Trading</li></a><a href="//historyas.github.io/gov.html"><li>Government</li></a></div> \
 <div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//historyas.github.io/Theme.css">'
 
